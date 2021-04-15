021054865a6eab2351376781598b17d7476f831d
妙灵科技 <m1aoling@qq.com>
2021-04-15 23:57:23 +0800

    轮播图逻辑的完善

--- Example/PHCarouselView/ViewController.swift
+++ Example/PHCarouselView/ViewController.swift
@@ -7,11 +7,31 @@
 //
 
 import UIKit
+import PHCarouselView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let carousel : PHCarouselView = PHCarouselView.init()
+        self.view.addSubview(carousel)
+        carousel.snp.makeConstraints { (make) in
+            make.top.equalTo(50)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(300)
+        }
+        carousel.numberCell = {
+            return 5
+        }
+        carousel.cellForIndex = {(page)in
+            let btn : UIButton = UIButton.init()
+            btn.setTitle("penghui", for: .normal)
+            btn.backgroundColor = UIColor.red
+           return btn
+        }
+        carousel.reload()
+        carousel.startAutoScroll()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ Example/Pods/Target Support Files/PHCarouselView/PHCarouselView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PHCarouselView : NSObject
+@end
+@implementation PodsDummy_PHCarouselView
+@end
